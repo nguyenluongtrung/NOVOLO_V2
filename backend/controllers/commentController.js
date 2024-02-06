18e43df3e42b30c1d9f7b8b733ffd603aa10559e
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-02-06 15:46:06 +0700

    Add some APIs for comment function

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -31,7 +31,98 @@ const replyComment = asyncHandler(async (req, res) => {
 	});
 });
 
+const deleteComment = asyncHandler(async (req, res) => {
+	const comment = await Comment.findById(req.params.commentId);
+
+	if (!comment) {
+		res.status(404);
+		throw new Error('Comment not found!');
+	}
+
+	await Comment.findByIdAndDelete(req.params.commentId);
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			id: req.params.commentId,
+		},
+	});
+});
+
+const getAllReplies = asyncHandler(async (req, res) => {
+	const comment = await Comment.findById(req.params.commentId);
+
+	if (!comment) {
+		res.status(404);
+		throw new Error('Comment not found!');
+	}
+
+	const replies = comment.replies;
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			replies,
+		},
+	});
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+	const comment = await Comment.findById(req.params.commentId);
+
+	if (!comment) {
+		res.status(404);
+		throw new Error('Comment not found!');
+	}
+
+	const updatedComment = await Comment.findByIdAndUpdate(
+		req.params.commentId,
+		req.body,
+		{ new: true }
+	);
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			updatedComment,
+		},
+	});
+});
+
+const getComment = asyncHandler(async (req, res) => {
+	const comment = await Comment.findById(req.params.commentId);
+
+	if (!comment) {
+		res.status(404);
+		throw new Error('Comment not found!');
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			comment,
+		},
+	});
+});
+
+const getAllComments = asyncHandler(async (req, res) => {
+	const comments = await Comment.find({});
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			length: comments.length,
+			comments,
+		},
+	});
+});
+
 module.exports = {
 	createComment,
 	replyComment,
+	deleteComment,
+	getAllReplies,
+	updateComment,
+	getComment,
+	getAllComments,
 };
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -2,11 +2,24 @@ const express = require('express');
 const {
 	createComment,
 	replyComment,
+	deleteComment,
+	getAllReplies,
+	updateComment,
+	getComment,
+	getAllComments,
 } = require('../controllers/commentController');
 const { protect } = require('../middleware/userMiddleware');
 const router = express.Router();
 
-router.route('/').post(protect, createComment);
-router.route('/:commentId').post(protect, replyComment);
+router.route('/').get(getAllComments).post(protect, createComment);
+router
+	.route('/reply/:commentId')
+	.get(getAllReplies)
+	.post(protect, replyComment);
+router
+	.route('/:commentId')
+	.get(getComment)
+	.delete(protect, deleteComment)
+	.patch(protect, updateComment);
 
 module.exports = router;
