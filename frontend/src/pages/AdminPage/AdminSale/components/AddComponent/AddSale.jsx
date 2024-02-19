36c5ee48b82f9d6d22d1b4a240401fdc9b704ba9
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-02-19 16:19:36 +0700

    Update add sale

--- frontend/src/pages/AdminPage/AdminSale/components/AddComponent/AddSale.jsx
+++ frontend/src/pages/AdminPage/AdminSale/components/AddComponent/AddSale.jsx
@@ -8,214 +8,219 @@ import { FaTimes } from 'react-icons/fa';
 import { getAllProducts } from '../../../../../features/products/productsSlice';
 import { useEffect, useState } from 'react';
 
-
-
 export const AddSale = ({ setIsOpenAddSaleForm }) => {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const [selectedProducts, setSelectedProducts] = useState([]);
+	const [selectedProducts, setSelectedProducts] = useState([]);
 
-    const {
-        products,
-        isError: productError,
-        isSuccess: productSuccess,
-        isLoading: productLoading,
-        message: productMessage,
-    } = useSelector((state) => state.products);
+	const {
+		products,
+		isError: productError,
+		isSuccess: productSuccess,
+		isLoading: productLoading,
+		message: productMessage,
+	} = useSelector((state) => state.products);
 
-    const {
-        isLoading: saleLoading,
-        isSuccess: saleSuccess,
-        isError: saleError,
-        message: saleMessage,
-    } = useSelector((state) => state.sales);
+	const {
+		isLoading: saleLoading,
+		isSuccess: saleSuccess,
+		isError: saleError,
+		message: saleMessage,
+	} = useSelector((state) => state.sales);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-    const handleCloseBtn = () => {
-        setIsOpenAddSaleForm(false);
-    };
+	const handleCloseBtn = () => {
+		setIsOpenAddSaleForm(false);
+	};
 
-    const onSubmit = async (data) => {
-        console.log(data);
-        const addSaleData = {
-            ...data,
-            productIds: selectedProducts.map((product) => product._id),
-        };
+	const onSubmit = async (data) => {
+		const addSaleData = {
+			...data,
+			productIds: selectedProducts,
+		};
 
-        if (saleError) {
-            toast.error(saleMessage);
-        }
+		await dispatch(createSale(addSaleData));
 
-        await dispatch(createSale(addSaleData));
+		if (saleSuccess) {
+			toast.success('Create sale successfully!');
+			handleCloseBtn();
+		}
+	};
 
-        if (saleSuccess) {
-            toast.success('Create sale successfully!');
-            handleCloseBtn();
-        }
-    };
-    useEffect(() => {
-        if (productError) {
-            toast.error(productMessage);
-        }
+	useEffect(() => {
+		if (productError) {
+			toast.error(productMessage);
+		}
 
-        Promise.all([
-            dispatch(getAllProducts('')),
-        ]).catch((error) => {
-            console.error('Error during dispatch:', error);
-        });
-    }, [dispatch, productError, productMessage]);
+		Promise.all([dispatch(getAllProducts(''))]).catch((error) => {
+			console.error('Error during dispatch:', error);
+		});
+	}, [dispatch, productError, productMessage]);
 
+	const handleProductDeselect = (productId) => {
+		const updatedSelectedProducts = selectedProducts.filter(
+			(product) => product !== productId
+		);
+		setSelectedProducts(updatedSelectedProducts);
+	};
 
+	if (saleLoading) {
+		return <Spinner />;
+	}
 
-    const handleProductDeselect = (productId) => {
-        const updatedSelectedProducts = selectedProducts.filter(
-            (product) => product._id !== productId
-        );
-        setSelectedProducts(updatedSelectedProducts);
-    };
+	return (
+		<div>
+			<div className="overlay-addSale"></div>
+			<div className="content">
+				<div className="addSaleForm">
+					<p
+						style={{
+							fontSize: '30px',
+							fontWeight: 'bold',
+							textAlign: 'center',
+						}}
+					>
+						ADD NEW PROMOTION
+					</p>
 
+					<div className="column">
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div
+								onClick={() => setIsOpenAddSaleForm(false)}
+								className="close-btn"
+							>
+								<FaTimes size={25} />
+							</div>
+							<div className="input-box">
+								<label>
+									Product <span className="text-danger">*</span>
+								</label>
+								<select
+									size={6}
+									{...register('productIds')}
+									multiple
+									onChange={(e) => {
+										setSelectedProducts([...selectedProducts, e.target.value]);
+									}}
+								>
+									{products.map((product) => {
+                                        if(selectedProducts.findIndex(
+											(selectedId) => selectedId == product._id
+										) == -1)
+										return (
+											<option key={product._id} value={product._id}>
+												{product.name}
+											</option>
+										);
+									})}
+								</select>
+							</div>
+							<div className="selected-products">
+								<p>Selected Products:</p>
+								<ul>
+									{selectedProducts.map((selectedId) => {
+										return (
+											<li key={selectedId}>
+												{
+													products[
+														products.findIndex(
+															(product) => product._id == selectedId
+														)
+													].name
+												}
+												<button
+													className="delete-btn-pname"
+													onClick={() => handleProductDeselect(selectedId)}
+												>
+													<FaTimes />
+												</button>
+											</li>
+										);
+									})}
+								</ul>
+							</div>
 
+							<div className="input-box">
+								<label>
+									Sale Name <span className="text-danger">*</span>
+								</label>
 
-    if (saleLoading) {
-        return <Spinner />;
-    }
+								<input
+									type="text"
+									{...register('promotionName')}
+									placeholder="Enter Sale name"
+									required
+								/>
 
+								<label>
+									Sale Value <span className="text-danger">*</span>
+								</label>
+								<input
+									type="text"
+									{...register('promotionValue')}
+									placeholder="Enter value"
+									required
+									min="0"
+								/>
+							</div>
 
+							<div className="input-box">
+								<label>
+									Start Date <span className="text-danger">*</span> &nbsp;
+									&nbsp;
+								</label>
+								<input
+									type="date"
+									{...register('startDate')}
+                                    min={new Date().toISOString().split('T')[0]}
+									required
+								/>
+								<label>
+									End Date <span className="text-danger">*</span> &nbsp; &nbsp;
+								</label>
+								<input
+									type="date"
+									{...register('endDate')}
+                                    min={new Date().toISOString().split('T')[0]}
+									required
+								/>
+							</div>
 
+							<div className="input-box">
+								<label>
+									Sale code <span className="text-danger">*</span> &nbsp; &nbsp;
+									<span className="text-danger" id="error-image"></span>
+								</label>
+								<input
+									type="text"
+									{...register('promotionCode')}
+									placeholder="Enter sale code"
+									required
+								/>
+								<label>
+									Quantity <span className="text-danger">*</span> &nbsp; &nbsp;
+									<span className="text-danger" id="error-image"></span>
+								</label>
+								<input
+									type="number"
+									{...register('promotionQuantity')}
+									placeholder="Enter sale code"
+									required
+								/>
+							</div>
 
-    return (
-        <div>
-            <div className="overlay-addSale"></div>
-            <div className="content">
-                <div className="addSaleForm">
-                    <p style={{ fontSize: '30px', fontWeight: 'bold', textAlign: 'center' }}>
-                        ADD NEW A SALE
-                    </p>
-                   
-                    <div className="column">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                        <div onClick={() => setIsOpenAddSaleForm(false)} className="close-btn">
-                        <FaTimes size={25} />
-                    </div>
-                            <div className="input-box">
-
-                                <label>
-                                    Product <span className="text-danger">*</span>
-                                </label>
-                                <select size={6}
-                                    {...register('productIds')}
-                                    multiple 
-                                    onChange={(e) => {
-                                        const selectedOptions = Array.from(e.target.selectedOptions, (option) =>
-                                            option.value
-                                        );
-                                        setSelectedProducts(
-                                            products.filter((product) => selectedOptions.includes(product._id))
-                                        );
-                                    }}
-                                >
-                                    {products.map((product) => (
-                                        <option key={product._id} value={product._id}>
-                                            {product.name}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className='selected-products'>
-                                <p>Selected Products:</p>
-                                <ul>
-                                    {selectedProducts.map((product) => (
-                                        <li key={product._id}>
-                                            {product.name}{' '}
-                                            <button className="delete-btn-pname" onClick={() => handleProductDeselect(product._id)}><FaTimes /></button>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-
-                            <div className="input-box">
-
-                                <label>
-                                    Sale Name <span className="text-danger">*</span>
-                                </label>
-
-                                <input
-                                    type="text"
-                                    {...register('promotionName')}
-                                    placeholder="Enter Sale name"
-                                    required
-                                />
-
-                                <label>
-                                    Sale Value <span className="text-danger">*</span>
-                                </label>
-                                <input
-                                    type="text"
-                                    {...register('promotionValue')}
-                                    placeholder="Enter value"
-                                    required
-                                    min="0"
-                                />
-
-
-                            </div>
-
-                            <div className="input-box">
-                                <label>
-                                    Start Date <span className="text-danger">*</span> &nbsp; &nbsp;
-                                </label>
-                                <input
-                                    type="date"
-                                    {...register('startDate')}
-                                    placeholder="Set start date"
-                                    required
-                                />
-                                <label>
-                                    End Date <span className="text-danger">*</span> &nbsp; &nbsp;
-                                </label>
-                                <input
-                                    type="date"
-                                    {...register('endDate')}
-                                    placeholder="Set end date"
-                                    required
-                                />
-                            </div>
-
-                            <div className="input-box">
-                                <label>
-                                    Sale code <span className="text-danger">*</span> &nbsp; &nbsp;
-                                    <span className="text-danger" id="error-image"></span>
-                                </label>
-                                <input
-                                    type="text"
-                                    {...register('promotionCode')}
-                                    placeholder="Enter sale code"
-                                    required
-                                />
-                                <label>
-                                    Quantity <span className="text-danger">*</span> &nbsp; &nbsp;
-                                    <span className="text-danger" id="error-image"></span>
-                                </label>
-                                <input
-                                    type="number"
-                                    {...register('promotionQuantity')}
-                                    placeholder="Enter sale code"
-                                    required
-                                />
-                            </div>
-
-                            <button className="submit-sale" type="submit">Submit</button>
-                        </form>
-                    </div>
-
-
-                </div>
-            </div >
-        </div >
-    )
-}
+							<button className="submit-sale" type="submit">
+								Submit
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
