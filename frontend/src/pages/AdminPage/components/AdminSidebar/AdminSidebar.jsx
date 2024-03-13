c95bfa1c8cd7ffbea79690fe2817f964fab25ae8
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-03-13 20:22:35 +0700

    Update css sidebar

--- frontend/src/pages/AdminPage/components/AdminSidebar/AdminSidebar.jsx
+++ frontend/src/pages/AdminPage/components/AdminSidebar/AdminSidebar.jsx
@@ -1,50 +1,58 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaUserSecret, FaTachometerAlt, FaGift, FaPowerOff, FaCreditCard, FaUtensils } from 'react-icons/fa'
 import './AdminSidebar.css';
 
 export const AdminSidebar = () => {
+	const location = useLocation();
+
+	const isDashboardActive = location.pathname === '/admin-dashboard';
+	const isProductsActive = location.pathname === '/admin-product';
+	const isOrdersActive = location.pathname === '/admin-order';
+	const isPromotionsActive = location.pathname === '/admin-promotion';
+	const isComboActive = location.pathname === '/admin-combo';
+
 	return (
 		<div>
-			<div className="bg-white" id="sidebar-wrapper">
-				<div className="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
-					<FaUserSecret />MANAGER PAGE
+			<div className="bg-sidebar" id="sidebar-wrapper" style={{backgroundColor: '#c4e4cb'}}>
+				<div className="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom text-sidebar">
+					<FaUserSecret /> MANAGER PAGE
 				</div>
 				<div className="list-group list-group-flush my-3">
 					<Link
                         to={'/admin-dashboard'}
-						className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+						className={`${isDashboardActive ? 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate item-active' : 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate'}`}
 					>
-						<FaTachometerAlt />Dashboard
+						<FaTachometerAlt /> &nbsp;Dashboard
 					</Link>
 					<Link
                         to={'/admin-product'}
-						className="list-group-item list-group-item-action bg-transparent second-text active fw-bold"
+						className={`${isProductsActive ? 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate item-active' : 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate'}`}
 					>
-						<FaTachometerAlt />Products
+						<FaTachometerAlt /> &nbsp;Products
 					</Link>
 					<Link
                         to={'/admin-order'}
-						className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+						className={`${isOrdersActive ? 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate item-active' : 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate'}`}
 					>
-						<FaGift />Orders
+						<FaGift /> &nbsp;Orders
 					</Link>
 					<Link
                         to={'/admin-promotion'}
-						className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+						className={`${isPromotionsActive ? 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate item-active' : 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate'}`}
 					>
-						<FaCreditCard />Promotions
+						<FaCreditCard /> &nbsp;Promotions
 					</Link>
 					<Link
                         to={'/admin-combo'}
-						className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+						className={`${isComboActive ? 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate item-active' : 'list-group-item list-group-item-action bg-transparent second-text fw-bold item-navigate'}`}
 					>
-						<FaUtensils />Combo
+						<FaUtensils /> &nbsp;Combo
 					</Link>
 					<Link
                         to={'/home'}
-						className="list-group-item list-group-item-action bg-transparent text-danger fw-bold"
+						className="list-group-item list-group-item-action bg-transparent text-danger fw-bold item-navigate"
 					>
-						<FaPowerOff />Home page
+						<FaPowerOff /> &nbsp;Home page
 					</Link>
 				</div>
 			</div>
