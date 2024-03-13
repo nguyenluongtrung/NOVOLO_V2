4311f01cdb10d0d93c229780a84aab0b64e2e683
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-03-13 20:24:45 +0700

    Update css

--- frontend/src/pages/AdminPage/AdminOrder/AdminOrder.jsx
+++ frontend/src/pages/AdminPage/AdminOrder/AdminOrder.jsx
@@ -23,28 +23,6 @@ export const AdminOrder = () => {
 			<AdminSidebar />
 
 			<div id="page-content-wrapper">
-				<nav className="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
-					<div className="d-flex align-items-center">
-						<i
-							className="fas fa-align-left primary-text fs-4 me-3"
-							id="menu-toggle"
-						></i>
-						<h2 className="headerOrder fs-2 m-0">Order Management</h2>
-					</div>
-
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-				</nav>
-
 				<div className="container-fluid px-4">
 					<div className="row my-5">
 						<div className="row mb-3 blockitem">
--- frontend/src/pages/AdminPage/AdminPromotion/AdminPromotion.jsx
+++ frontend/src/pages/AdminPage/AdminPromotion/AdminPromotion.jsx
@@ -70,28 +70,6 @@ export const AdminPromotion = () => {
 				<AdminSidebar />
 
 				<div id="page-content-wrapper">
-					<nav className="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
-						<div className="d-flex align-items-center">
-							<i
-								className="fas fa-align-left primary-text fs-4 me-3"
-								id="menu-toggle"
-							></i>
-							<h2 className="fs-2 m-0">Promotion Management</h2>
-						</div>
-
-						<button
-							className="navbar-toggler"
-							type="button"
-							data-bs-toggle="collapse"
-							data-bs-target="#navbarSupportedContent"
-							aria-controls="navbarSupportedContent"
-							aria-expanded="false"
-							aria-label="Toggle navigation"
-						>
-							<span className="navbar-toggler-icon"></span>
-						</button>
-					</nav>
-
 					<div className="container-fluid px-4">
 						<div className="row my-5">
 							<h3 className="fs-4 mb-3 d-inline col-sm-10">Promotion list</h3>
